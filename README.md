c542049886411c4133a63467e769eeaa56cc1693
ErickPaulSweeney <erickpaulsweeney@gmail.com>
2022-09-01 15:37:36 +0800

    Done so far

---
+++ README.md
@@ -0,0 +1,10 @@
+# Multiplayer Tic Tac Toe (Backend)
+
+The classic game you can share and play with your friends by simply sharing a game room!
+
+### Packages used
+- cors
+- dotenv
+- express
+- morgan
+- socket.io
---
+++ server.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const app = express();
+
+app.use(cors());
+app.use(express.static("public"));
+app.use(morgan("dev"));
+
+const httpServer = app.listen(process.env.PORT || 8000, () =>
+    console.log("Server connected")
+);
+
+const io = require("socket.io")(httpServer, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    // console.log("Client connnected", socket.id);
+    socket.on("turn-switch", (data) => {
+        const room = JSON.parse(data).room;
+        io.to(room).emit("opponent-move", data);
+    });
+
+    socket.on("create", (room) => {
+        socket.join(room);
+    });
+
+    socket.on("join", (room) => {
+        socket.join(room);
+        io.to(room).emit("game-start");
+    });
+
+    socket.on("restart", (data) => {
+        const room = JSON.parse(data).room;
+        io.to(room).emit("restart");
+    });
+});
